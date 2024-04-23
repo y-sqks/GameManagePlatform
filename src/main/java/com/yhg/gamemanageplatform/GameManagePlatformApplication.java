dc5a4f2cbdfafd1109d46930dbc8e041a6ccb305
ks <3315224354@qq.com>
2024-04-23 16:36:20 +0800

    Submit to test

---
+++ src/main/java/com/yhg/gamemanageplatform/GameManagePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yhg.gamemanageplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameManagePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameManagePlatformApplication.class, args);
+	}
+
+}
